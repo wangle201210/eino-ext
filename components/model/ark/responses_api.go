bd6375a0b3cfc13dc276cc11817d76db2b91ace0
mrh997 <maronghong@bytedance.com>
2025-11-17 17:04:52 +0800

    fix(model_ark): ToolChoice cannot be passed when reading from the cache (#543)

--- components/model/ark/responses_api.go
+++ components/model/ark/responses_api.go
@@ -495,25 +495,8 @@ func (cm *responsesAPIChatModel) genRequestAndOptions(in []*schema.Message, opti
 		return nil, err
 	}
 
-	if err = cm.populateTools(reqParams.req, options.Tools); err != nil {
+	if err = cm.populateTools(reqParams.req, options.Tools, options.ToolChoice); err != nil {
 		return nil, err
-	}
-
-	if options.ToolChoice != nil {
-		var tco responses.ToolChoiceOptions
-		switch *options.ToolChoice {
-		case schema.ToolChoiceForbidden:
-			tco = responses.ToolChoiceOptionsNone
-		case schema.ToolChoiceAllowed:
-			tco = responses.ToolChoiceOptionsAuto
-		case schema.ToolChoiceForced:
-			tco = responses.ToolChoiceOptionsRequired
-		default:
-			tco = responses.ToolChoiceOptionsAuto
-		}
-		reqParams.req.ToolChoice = responses.ResponseNewParamsToolChoiceUnion{
-			OfToolChoiceMode: param.NewOpt(tco),
-		}
 	}
 
 	for k, v := range specOptions.customHeaders {
@@ -840,7 +823,7 @@ func (cm *responsesAPIChatModel) toOpenaiMultiModalContent(msg *schema.Message) 
 	return content, nil
 }
 
-func (cm *responsesAPIChatModel) populateTools(req *responses.ResponseNewParams, optTools []*schema.ToolInfo) error {
+func (cm *responsesAPIChatModel) populateTools(req *responses.ResponseNewParams, optTools []*schema.ToolInfo, toolChoice *schema.ToolChoice) error {
 	// When caching is enabled, the tool is only passed on the first request.
 	if req.PreviousResponseID.Valid() {
 		return nil
@@ -856,6 +839,23 @@ func (cm *responsesAPIChatModel) populateTools(req *responses.ResponseNewParams,
 	}
 
 	req.Tools = tools
+
+	if toolChoice != nil {
+		var tco responses.ToolChoiceOptions
+		switch *toolChoice {
+		case schema.ToolChoiceForbidden:
+			tco = responses.ToolChoiceOptionsNone
+		case schema.ToolChoiceAllowed:
+			tco = responses.ToolChoiceOptionsAuto
+		case schema.ToolChoiceForced:
+			tco = responses.ToolChoiceOptionsRequired
+		default:
+			tco = responses.ToolChoiceOptionsAuto
+		}
+		req.ToolChoice = responses.ResponseNewParamsToolChoiceUnion{
+			OfToolChoiceMode: param.NewOpt(tco),
+		}
+	}
 
 	return nil
 }
